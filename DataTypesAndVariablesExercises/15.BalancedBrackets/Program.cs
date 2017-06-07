5958c1f81b101d37d7638b48444d8b35e6419ade
RoziPetkova <savana_93@abv.bg>
2017-06-07 11:30:55 +0300

    Signed-off-by: RoziPetkova <savana_93@abv.bg> 15.BalancedBrachets

---
+++ DataTypesAndVariablesExercises/15.BalancedBrackets/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.BalancedBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            string checker = "";
+            for (int i = 0; i < input; i++)
+            {
+                string strToCheck = Console.ReadLine();
+                if (strToCheck == "(" || strToCheck == ")") checker = checker + strToCheck;                
+            }
+
+            int countOpening = 0;
+            for (int i = 0; i < checker.Length; i++)
+            { if(checker[i] == '(') countOpening++; }
+
+            int countClosing = 0;
+            for (int i = 0; i < checker.Length ; i++)
+            { if(checker[i] == ')') countClosing++; }
+
+            if (countClosing == countOpening && checker[checker.Length-1].Equals(")"))
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else Console.WriteLine("UNBALANCED");
+        }
+    }
+}
